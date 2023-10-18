3112950db2eb1d302b85f6f0d20c507661af08ca
Fedor <starbreeze@yandex.ru>
2023-10-18 03:18:52 -0700

    Обновлена логика

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // Инициализируем переменную определяющую статус состояния экрана
+    var lightOn = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        updateUI()
     }
 
-
+    fileprivate func updateUI() {
+        view.backgroundColor = lightOn ? .white : .black
+    }
+    
+    // Делаем привязку действия buttonPressed с элементом интерфейса экранна Button
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        lightOn.toggle()
+        updateUI()
+    }
+    
 }
 
